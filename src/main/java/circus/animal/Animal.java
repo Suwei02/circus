a62f69e4fd523c121fafba308fc59a604cb59488
Suwei <shresthasuwei@gmail.com>
2026-02-06 17:42:49 +0800

    Updated code based on lect instructioins

---
+++ src/main/java/circus/animal/Animal.java
@@ -0,0 +1,9 @@
+package circus.animal;
+
+import circus.Asset;
+
+public abstract class Animal implements Asset {
+
+    public abstract String speak();
+    //not mandatory for the method to be abstract
+}
